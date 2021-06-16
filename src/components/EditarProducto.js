befef1113dd60690898c53ce659120054ecd257d
Kevin <kalonso.restrepo@udea.edu.co>
2021-06-16 18:55:03 -0500

    First time with React-Redux

---
+++ src/components/EditarProducto.js
@@ -0,0 +1,86 @@
+import React,{useState, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {editarProductoAction} from '../actions/productoActions';
+import {useHistory} from 'react-router-dom';
+
+const EditarProducto = () => {
+
+    const history = useHistory();
+
+    //nuevo state de producto
+    const [producto, guardarproducto] =useState({
+        nombre:'', 
+        precio:''
+    })
+    const dispatch = useDispatch();
+
+    //producto a editar
+    const productoeditar = useSelector( state => state.productos.productoeditar );
+
+    //llenar el state automaticamente con useEffect
+    useEffect(() => {
+        guardarproducto(productoeditar)
+    }, [productoeditar])
+
+    //leer los datos del formulario
+    const onChangeFormulario = e =>{
+        guardarproducto({
+            ...producto,
+            [e.target.name] : e.target.value
+        })
+    }
+    const {nombre,precio} = producto;
+
+    const submitEditarProducto = e =>{
+        e.preventDefault();
+
+        dispatch( editarProductoAction(producto) );
+        history.push('/');
+    }
+    return (  
+        <div className='row justify-content-center'>
+            <div className='col-md-8'>
+                <div className='card'>
+                    <div className='card-body'>
+                        <h2 className='text-center mb-4 font-weight-bold'>
+                            Editar Producto
+                        </h2>
+
+                        <form
+                            onSubmit={submitEditarProducto}
+                        >
+                            <div className='form-group'>
+                                <label>Nombre de Producto</label>
+                                <input 
+                                    type='text'
+                                    className='form-control'
+                                    placeHolder='Nombre Producto'
+                                    name='nombre'
+                                    value={nombre}
+                                    onChange={onChangeFormulario}
+                                />
+                            </div>
+                            <div className='form-group'>
+                                <label>Precio del Producto</label>
+                                <input 
+                                    type='number'
+                                    className='form-control'
+                                    placeHolder='Precio Producto'
+                                    name='precio'
+                                    value={precio}
+                                    onChange={onChangeFormulario}
+                                />
+                            </div>
+                            <button 
+                                type='submit'
+                                className='btn btn-primary font-weight-bold text-uppercase d-block w-100'
+                            >Guardar Cambios</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default EditarProducto;
